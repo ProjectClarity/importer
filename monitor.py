5594552a166c75b1a88a61791a4b2e1d8bbf7a2e
Yasyf Mohamedali <yasyfm@gmail.com>
2014-09-13 04:49:11 -0400

    Better logging

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import time, base64
 from remote import *
 from user import User
+from helpers import send_to_queue
 
 while True:
   for u in users.find({}):
@@ -27,7 +28,10 @@ while True:
       del message['payload']['parts']
       message['payload']['body'] = base64.b64decode(body)
       try:
-        print message
+        object_id = raw_data.insert(message)
+        send_to_queue({'object_id': str(object_id)})
+        print "Processed {}".format(message['id'])
       except:
+        print "Ignored duplicate {}".format(message['id'])
         continue
-  time.sleep(1)
+  time.sleep(30)
